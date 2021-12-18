362545bfc00c9855c42d6d6ef3f4dd3eaab13fe7
Raphael Raberger <Raphael.Raberger@htl-wels.at>
2021-12-18 11:39:50 +0100

    Projektdateien hinzufügen.

---
+++ AIFakeNews/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace AIFakeNews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WebManager.textfromwebsite("yes"));
+        }
+
+       
+    }
+}
---
+++ AIFakeNews/WebManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using mshtml;
+
+namespace AIFakeNews
+{
+    public class WebManager
+    {
+
+       public static string textfromwebsite(string website)
+        {
+
+            var client = new WebClient();
+
+            var s = client.DownloadString(@"https://stackoverflow.com/questions/7264659/read-text-from-web-page");
+            var htmldoc2 = (IHTMLDocument2)new HTMLDocument();
+            htmldoc2.write(s);
+            var plainText = htmldoc2.body.outerText;
+            return plainText;
+        }
+
+
+    }
+}
