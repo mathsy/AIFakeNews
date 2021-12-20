1e15b3d292d30877c3fc618cb597ecf18c97ff55
Raphael Raberger <Raphael.Raberger@htl-wels.at>
2021-12-20 19:33:21 +0100

    Relative data path added

--- AIFakeNews/Program.cs
+++ AIFakeNews/Program.cs
@@ -7,11 +7,14 @@ namespace AIFakeNews
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(WebManager.textfromwebsite(@"https:\\de.wikipedia.org"));
+           // Console.WriteLine(WebManager.textfromwebsite(@"https:\\de.wikipedia.org"));
             foreach(string s in WebManager.linksfromwebsite(@"https:\\de.wikipedia.org"))
             {
-                Console.WriteLine(s);
+                //Console.WriteLine(s);
             }
+
+            Console.WriteLine(System.IO.File.ReadAllText("data/topicdata.csv"));
+
 
             TopicalAI.start_AI(true);
             TopicalAI.start_AI(false);
--- AIFakeNews/TopicalAI.cs
+++ AIFakeNews/TopicalAI.cs
@@ -12,7 +12,7 @@ namespace AIFakeNews
         public static void start_AI(bool train = false)
         {
             var context = new MLContext();
-            var data = context.Data.LoadFromTextFile<TopicData>("topicdata.csv", hasHeader: true, separatorChar: ';');
+            var data = context.Data.LoadFromTextFile<TopicData>("data/topicdata.csv", hasHeader: true, separatorChar: ';');
             if (train)
             {
                 var pipeline = context.Transforms.Expression("Label", "(x) => x== 1 ? true : false", "Similiarity")
@@ -20,7 +20,7 @@ namespace AIFakeNews
                     .Append(context.Transforms.Text.FeaturizeText("Features", nameof(TopicData.Text2)))
                     .Append(context.BinaryClassification.Trainers.SdcaLogisticRegression());
                 var model = pipeline.Fit(data);
-                context.Model.Save(model, data.Schema, "topic_ai.zip");
+                context.Model.Save(model, data.Schema, "topic_ai.txt");
                 var predictionEngine = context.Model.CreatePredictionEngine<TopicData, TopicPrediction>(model);
                 var prediction = predictionEngine.Predict(new TopicData { Text1 = "Flat earth is definitley true.", Text2 = "As we can see, flat earth is true!" });
                 Console.WriteLine(prediction.Prediction + " " + prediction.Probability);
@@ -29,9 +29,9 @@ namespace AIFakeNews
             if(!train)
             {
                 DataViewSchema modelSchema;
-                var model = context.Model.Load("topic_ai.zip", out modelSchema);
+                var model = context.Model.Load("topic_ai.txt", out modelSchema);
                 var predictionEngine = context.Model.CreatePredictionEngine<TopicData, TopicPrediction>(model);
-                foreach(string l in System.IO.File.ReadAllLines("topicdata.csv"))
+                foreach(string l in System.IO.File.ReadAllLines("data/topicdata.csv"))
                 {
                     var prediction = predictionEngine.Predict(new TopicData { Text1 = l.Split(';')[0], Text2 = l.Split(';')[1] });
                     Console.WriteLine( l + " " + prediction.Prediction + " " + prediction.Probability);
